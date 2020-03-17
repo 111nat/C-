0957caeff819d8f7ae6be7cbf0f899f069da63d1
Alex <alexcs2013@mail.ru>
2020-03-17 16:56:27 +0300

    Add matrix/

---
+++ matrix/Matrix.cpp
@@ -0,0 +1,94 @@
+#include "Matrix.h"
+void Matrix::print() {
+	for (int i = 0; i < 9; i++) {
+		if (i == 3 || i == 6) {
+			cout<<endl;
+		}
+		cout << " "<<arr[i];
+	}
+	cout <<endl<< "end of print"<<endl;
+}
+
+Matrix operator+(Matrix const& a, Matrix const& b)
+{
+	Matrix result = Matrix();
+	for (int i = 0; i < 9; i++) {
+		result.arr[i] = a.arr[i] + b.arr[i];
+	}
+	return result;
+}
+
+Matrix operator*(Matrix const& a, Matrix const& b)
+{
+	Matrix result = Matrix();
+	for (int l = 0; l < 3; l++) {
+		for (int i = 0; i < 3; i++) {
+			result.arr[i+(l*3)] = a.arr[0 + (l * 3)] * b.arr[i] + a.arr[1 + (l * 3)] * b.arr[i + 3] + a.arr[2 + (l * 3)] * b.arr[i + 6];
+		}
+	}
+
+	
+
+
+	return result;
+}
+
+Matrix operator*(Matrix const& a, int const& b) {
+	Matrix result = Matrix();
+	for (int i = 0; i < 9; i++) {
+		result.arr[i] = a.arr[i] * b;
+	}
+
+
+
+
+	return result;
+}
+
+void Matrix::ch() {
+	for (int i = 0; i < 10; i++) {
+		arr[i] = i;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+//class Matrix {
+
+
+
+
+/*public:
+	double*arr;
+	Matrix(int len) {
+		arr = new double[9];
+	}
+
+	friend Matrix operator+(Matrix const& a, Matrix const& b) {
+		Matrix result = Matrix();
+		for (int i = 0; i < 10; i++) {
+			result.arr[i] = a.arr[i] + b.arr[i];
+		}
+		return result;
+	}
+	void ch() {
+		for (int i = 0; i < 10; i++) {
+			arr[i] = i;
+		}
+	}
+	void print() {
+		for (int i = 0; i < 10; i++) {
+			cout << arr[i] << endl;
+		}
+	}
+	*/
+//};
+
+
---
+++ matrix/Matrix.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+class Matrix{
+public:
+	double* arr=new double[9];
+	//Matrix() {
+	//	arr = new double[9];
+	//}
+	friend Matrix operator+(Matrix const& a, Matrix const& b);
+	friend Matrix operator*(Matrix const& a, Matrix const& b);
+	friend Matrix operator*(Matrix const& a, int const& b);
+	void print();
+	void ch();
+};
+
