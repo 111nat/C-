25346669b9cb3e7d0faed07dbc05a8d753013a25
Alex <alexcs2013@mail.ru>
2020-03-17 18:52:11 +0300

    last

--- matrix/Matrix.h
+++ matrix/Matrix.h
@@ -6,9 +6,7 @@ using std::endl;
 class Matrix{
 public:
 	double* arr=new double[9];
-	//Matrix() {
-	//	arr = new double[9];
-	//}
+	
 	friend Matrix operator+(Matrix const& a, Matrix const& b);
 	friend Matrix operator*(Matrix const& a, Matrix const& b);
 	friend Matrix operator*(Matrix const& a, int const& b);
